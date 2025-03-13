ef9b891a741d54f822fc0599645e28a3376bc90d
marvinalnashi <marvinalnashi@hotmail.com>
2025-03-14 00:07:22 +0100

    Fixes

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "wss://little-chefs-cookbook-production.up.railway.app/ws";
+const SOCKET_URL = process.env.NEXT_PUBLIC_BACKEND_URL + "/ws" || "wss://little-chefs-cookbook-production.up.railway.app/ws";
 
 export const socket = io(SOCKET_URL, {
     transports: ["websocket"],
