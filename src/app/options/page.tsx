623c9667d6d688b09e0de2635e647b90d2870de5
marvinalnashi <marvinalnashi@hotmail.com>
2025-04-04 19:04:14 +0200

    Added button in the Options view for clearing localstorage

--- src/app/options/page.tsx
+++ src/app/options/page.tsx
@@ -24,8 +24,20 @@ export default function Page() {
                 onClick={toggleTheme}
                 className="bg-[#1E88E5] hover:bg-[#1565C0] text-white flex items-center justify-between gap-2 w-full max-w-xs px-4 py-4 rounded-2xl text-lg font-bold mb-4 transition-all"
             >
-                <span className="text-2xl">🏠</span>
+                <span className="text-2xl">{theme === "light" ? "🌕" : "☀️"}</span>
                 <span className="flex-1 text-center">{theme === "light" ? "Dark" : "Light"} Mode</span>
+                <span className="text-sm">🔍</span>
+            </button>
+            <button
+                className="flex items-center justify-between gap-2 w-full max-w-xs px-4 py-4 rounded-2xl bg-[#FCA5A5] text-lg font-bold mb-4 transition-all hover:bg-[#EF4444]"
+                onClick={() => {
+                    localStorage.removeItem("includedIngredients");
+                    localStorage.removeItem("excludedIngredients");
+                    localStorage.removeItem("selectedOccasion");
+                }}
+            >
+                <span className="text-2xl">💾</span>
+                <span className="flex-1 text-center">Clear Localstorage</span>
                 <span className="text-sm">🔍</span>
             </button>
         </div>
